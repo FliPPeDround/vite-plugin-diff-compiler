fc2a369f6baf5ea080befb22a1b4458d67e98f72
flippedround <734243792@qq.com>
2023-02-06 15:11:31 +0800

    chore: release 1.0.1

---
+++ test/fixtures/basic.js
@@ -0,0 +1,7 @@
+// #diff-compiler-start:js
+console.log('Hello js')
+// #diff-compiler-end:js
+
+// #diff-compiler-start:basic
+console.log('Hello basic')
+// #diff-compiler-end:basic
---
+++ test/fixtures/vue3.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+// #diff-compiler-start:node
+console.log('Hello node')
+// #diff-compiler-end:node
+
+// #diff-compiler-start:vue3
+console.log('Hello vue3')
+// #diff-compiler-end:vue3
+</script>
+
+<template>
+  <!-- #diff-compiler-start:node -->
+  <h1>Node</h1>
+  <!-- #diff-compiler-end:node -->
+  <!-- #diff-compiler-start:vue3 -->
+    <h1>vue3</h1>
+  <!-- #diff-compiler-end:vue3 -->
+</template>
+
+<style>
+/* #diff-compiler-start:node */
+h1 {
+  color: red;
+}
+/* #diff-compiler-end:node */
+/* #diff-compiler-start:vue3 */
+h1 {
+  color: blue;
+}
+/* #diff-compiler-end:vue3 */
+</style>
