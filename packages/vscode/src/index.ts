97046602fbf7eebd3b42f2473c50c6c4281d36b6
flippedround <734243792@qq.com>
2023-02-08 11:23:00 +0800

    chore: init shared package

--- packages/vscode/src/index.ts
+++ packages/vscode/src/index.ts
@@ -1,15 +1,18 @@
 import type { ExtensionContext } from 'vscode'
 import { commands, window } from 'vscode'
-import { version } from '../package.json'
-import { log } from './log'
+// import { version } from '../package.json'
+// import { log } from './log'
 
 export function activate(context: ExtensionContext) {
-  log.appendLine(`⚪️ Diff Compiler for VS Code v${version}\n`)
-  const disposable = commands.registerCommand('diffCompiler.reload', () => {
+  const code = editor.document.getText()
+  console.log(data)
+  // log.appendLine(`⚪️ Diff Compiler for VS Code v${version}\n`)
+  commands.registerCommand('diffCompiler.reload', () => {
+    console.log('test')
     window.showInformationMessage('test')
   })
 
-  context.subscriptions.push(disposable)
+  // context.subscriptions.push(disposable)
 }
 
 export function deactivate() {}
---
+++ packages/vscode/test/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+
+describe('test', () => {
+  it('test', () => {
+    expect(1).toBe(1)
+  })
+})
+
